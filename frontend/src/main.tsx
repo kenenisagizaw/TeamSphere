614f2785175a66e53a0249721f69f96e3c05bb59
Kenenisa Gizaw <152353954+kenenisagizaw@users.noreply.github.com>
2026-02-13 02:15:42 +0300

    refactor: restructure main.tsx to improve component hierarchy and organization

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.tsx";
+import { AuthProvider } from "./context/AuthContext";
+import { SocketProvider } from "./context/SocketContext";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <SocketProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </SocketProvider>
+    </AuthProvider>
+  </StrictMode>
+);
